32fb945df56f87d92d3d17e856e66f7865825f3a
amir-ibrahim-us <amir.ibrahim.us@gmail.com>
2019-04-02 09:16:08 -0400

    initial commit

---
+++ KafkaTwitterStream/src/main/java/TwitterSourceConstant.java
@@ -0,0 +1,7 @@
+public class TwitterSourceConstant {
+        public static final String CONSUMER_KEY_KEY = "0eESOpZUztaif2yNvIIioI9lT";
+        public static final String CONSUMER_SECRET_KEY = "XfgigYczLipq4XOasNAtKNGasJVhvN7M0QQ0dG3siEQm8pxNPs";
+        public static final String ACCESS_TOKEN_KEY = "1105476682100682753-q6rYWbhWwIxYPIiCNYCCoDxEoMMJmP";
+        public static final String ACCESS_TOKEN_SECRET_KEY = "vPJuCLnkymCc4GpKrJ8jbiNVga68U8cBEWp72c2zZZw22";
+    }
+
