285923635096b679675324b0a990f758456f21ad
amir-ibrahim-us <49177041+amir-ibrahim-us@users.noreply.github.com>
2019-05-05 22:48:22 -0400

    Update config.py

--- Docker/config.py
+++ Docker/config.py
@@ -1,4 +1,4 @@
-consumer_key = "kmCDCNoodyq0lUn1oEEIgiTgX"
-consumer_secret = "fe0JwbnsTnn8ZDNR6elkf6uaIc8Nq4B4l9K3qaI2WxUXUp9cIz"
-access_token = "1105476682100682753-KRVupfheYOeeIW7OHXZzEgr8FMwKy8"
-access_token_secret = "jmOLlWmvwlCZzIPuwBPZpMgyo79ZBv8cdq0gzcQtmfL0K"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
